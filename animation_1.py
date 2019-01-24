50575c81f3f7f0ec3df0e35e78f461ccc68923bd
Deepak Pednekar <deepak.pednekar009@gmail.com>
2019-01-24 10:24:10 +0530

    INITIAL COMMIT

---
+++ animation_1.py
@@ -0,0 +1,30 @@
+import random , string , time , sys
+
+st = "()[]"
+# st = string.punctuation
+sys.stdout.write("Starting hack............ ")
+for _ in range(100):
+	# sys.stdout.write(random.choice(st))
+	print(random.choice(st) , end="" , flush=True)#, end="\r"
+	time.sleep(0.1)
+	sys.stdout.write("\b")
+	# sys.stdout.flush()
+
+sys.stdout.write("\n")
+# print("flusingin" , flush=True)
+# time.sleep(0.5)
+
+# for x in range (0,5):  
+#     b = "Loading" + "." * x
+#     print (b, end="\r")
+#     time.sleep(1)
+
+# msg = 'I am going to erase this line from the console window.'
+# sys.stdout.write(msg); sys.stdout.flush()
+# time.sleep(1)
+
+# sys.stdout.write('\r' + ' '*len(msg))
+# sys.stdout.flush()
+# time.sleep(0.5)
+# print()
+
